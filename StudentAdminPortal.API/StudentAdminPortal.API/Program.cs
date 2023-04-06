238a38d36eebf83e5a541febdf192e323735e17f
1MaDLorD1 <yroslav2001@list.ru>
2023-04-06 19:17:22 +0300

    Retrieving and Displaying Single Student

--- StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
@@ -5,7 +5,16 @@ using StudentAdminPortal.API.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors((options) =>
+{
+    options.AddPolicy("angularApplication", (builder) =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .WithMethods("GET", "POST", "PUT", "DELETE")
+        .WithExposedHeaders("*");
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -25,6 +34,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("angularApplication");
 
 app.UseAuthorization();
 
