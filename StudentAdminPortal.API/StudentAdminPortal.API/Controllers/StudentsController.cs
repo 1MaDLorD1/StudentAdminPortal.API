71db3001fb794c4224c3120503c8ad3a5d4c2980
1MaDLorD1 <yroslav2001@list.ru>
2023-04-13 23:07:37 +0300

    Server and Client Side Validations

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -85,18 +85,36 @@ namespace StudentAdminPortal.API.Controllers
         [Route("[controller]/{studentId:guid}/upload-image")]
         public async Task<IActionResult> UploadImage([FromRoute] Guid studentId, IFormFile profileImage)
         {
-            if (await studentRepository.Exists(studentId))
-            {
-                var fileName = Guid.NewGuid() + Path.GetExtension(profileImage.FileName);
-                
-                var fileImagePath = await imageRepository.Upload(profileImage, fileName);
+            var validExtensions = new List<string>
+            { 
+                ".jpeq",
+                ".png",
+                ".gif",
+                ".jpg"
+            };
 
-                if(await studentRepository.UpdateProfileImage(studentId, fileImagePath))
+            if (profileImage != null && profileImage.Length > 0)
+            {
+                var extension = Path.GetExtension(profileImage.FileName);
+
+                if(validExtensions.Contains(extension))
                 {
-                    return Ok(fileImagePath);
+                    if (await studentRepository.Exists(studentId))
+                    {
+                        var fileName = Guid.NewGuid() + Path.GetExtension(profileImage.FileName);
+
+                        var fileImagePath = await imageRepository.Upload(profileImage, fileName);
+
+                        if (await studentRepository.UpdateProfileImage(studentId, fileImagePath))
+                        {
+                            return Ok(fileImagePath);
+                        }
+
+                        return StatusCode(StatusCodes.Status500InternalServerError, "Ошибка загразки изображения");
+                    }
                 }
 
-                return StatusCode(StatusCodes.Status500InternalServerError, "Error uploading image");
+                return BadRequest("Неверный формат изображения");
             }
 
             return NotFound();
---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Validators/UpdateStudentRequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Repositories;
+
+namespace StudentAdminPortal.API.Validators
+{
+    public class UpdateStudentRequestValidator: AbstractValidator<UpdateStudentRequest>
+    {
+        public UpdateStudentRequestValidator(IStudentRepository studentRepository)
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.SecondName).NotEmpty();
+            RuleFor(x => x.DateOfBirth).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Mobile).GreaterThan(9999999).LessThan(100000000000);
+            RuleFor(x => x.GenderId).NotEmpty().Must(id =>
+            {
+                var gender = studentRepository.GetGendersAsync().Result.ToList()
+                .FirstOrDefault(x => x.Id == id);
+
+                if (gender != null)
+                {
+                    return true;
+                }
+
+                return false;
+            }).WithMessage("Пожалуйста выберите ваш пол");
+            RuleFor(x => x.PhysicalAddress).NotEmpty();
+            RuleFor(x => x.PostalAddress).NotEmpty();
+        }
+    }
+}
